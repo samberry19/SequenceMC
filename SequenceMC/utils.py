ed24e7f30d24d53595498c4b60ae1036bea53247
Sam Berry <61884003+samberry19@users.noreply.github.com>
2023-04-11 18:21:35 -0400

    added selector to utils

--- SequenceMC/utils.py
+++ SequenceMC/utils.py
@@ -229,3 +229,33 @@ def SequencePCA(alignment, n=2, sparse=False, alpha=1, ridge_alpha=0.01):
     alignment_latent_pca = sequence_pca.fit_transform(encoded_natural_sequences)
 
     return sequence_pca, alignment_latent_pca, encoded_natural_sequences
+
+def selector(df, dic):
+
+    '''A little tool for selecting from pandas dataframes by passing a dictionary, e.g.
+            selector(df, {"color":"red", "shape":["square", "circle"]})
+       
+        For advanced usage, you can pass a function and it will return where True, e.g.
+            selector(df, ["name": lambda name: "Sam" in name])
+           
+        You can also use this to select things greater than or less than a value, e.g.
+            selector(df, ["enrichment": lambda enr: enr > 1])'''
+   
+    X = df.copy()
+
+    for key,val in dic.items():
+       
+        # If you pass a tuple, list or numpy array
+        if isinstance(val, (tuple, list, np.ndarray)):
+            where = np.any(np.array([X[key]==v for v in val]),axis=0)
+            X = X.loc[where]
+           
+        # If you pass a function
+        elif isinstance(val, type(lambda x: x+1)):
+            X = X.loc[X[key].apply(val)]
+           
+        # Otherwise we assume it's a single value
+        else:
+            X = X.loc[X[key]==val]
+
+    return X
